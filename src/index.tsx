adc7a4c905ec10e17dda9e1a1fcf159bce8c6341
Thanh Pham <zlatan.pham@gmail.com>
2021-08-13 22:56:50 +0700

    fix: repeated field

--- src/index.tsx
+++ src/index.tsx
@@ -40,28 +40,37 @@ const getFilteredSuggestions = (
   inputValue: string,
   activeItem: Item,
 ) => {
+  const isArrayFilter = (item) => {
+    return (
+      activeItem?.type !== 'field' ||
+      (activeItem?.type === 'field' && !activeItem?.isArray) ||
+      (activeItem?.type === 'field' &&
+        activeItem?.isArray &&
+        item.supportIsArray)
+    );
+  };
+
   const filterFunc = (item) =>
     (item?.text?.toLowerCase().startsWith(inputValue.toLowerCase()) ||
       item?.value?.toLowerCase().startsWith(inputValue.toLowerCase())) &&
     (activeItem?.type !== 'field' ||
       (activeItem?.type === 'field' &&
         'types' in item &&
-        item?.types.includes(activeItem.fieldType))) &&
-    (activeItem?.type !== 'field' ||
-      (activeItem?.type === 'field' &&
-        'supportIsArray' in item &&
-        (!item.supportIsArray || (item.supportIsArray && activeItem.isArray))));
+        item?.types.includes(activeItem.fieldType)));
 
   // Group case
   if (items[0]?.title) {
     return items
       .map((group) => {
-        return { ...group, items: group.items.filter(filterFunc) };
+        return {
+          ...group,
+          items: group.items.filter(isArrayFilter).filter(filterFunc),
+        };
       })
       .filter(({ items }) => items.length > 0);
   }
 
-  return items.filter(filterFunc);
+  return items.filter(isArrayFilter).filter(filterFunc);
 };
 
 const flattenSuggestions = (items: any[]) => {
